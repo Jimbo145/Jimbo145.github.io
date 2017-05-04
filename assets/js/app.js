a0d62574cee11de1fb43abd3d38440312bf883ab
Jimbo145 <cpickering145@gmail.com>
2017-05-03 21:56:51 -0400

    completed placeholder website

---
+++ assets/js/app.js
@@ -0,0 +1,1 @@
+angular.module('colePickering', ['mm.foundation', 'ngAnimate', 'ngResource', 'ngSanitize']);
---
+++ assets/js/cole-pickering.js
@@ -0,0 +1,7 @@
+(function(){
+    'use strict';
+
+angular.module('colePickering', ['mm.foundation', 'ngAnimate', 'ngResource', 'ngSanitize']);
+
+
+}());
